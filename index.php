edc4635473a4d89d6148cd0250cdaa13c2eaef58
Ilya <edmundeas@gmail.com>
2017-02-11 06:13:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,14 +7,16 @@
 	 */
 	 
 	 $usr = "root";
-	 $pswrd = "123trus";
+	 $pswrd = "";
+
 	 $config = array(PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC);
 	 $db_connect = new PDO('mysql:host=localhost;dbname=cms', $usr,$pswrd,$config);
 	 $stmt = $db_connect->query('SELECT* From user,message');
+
 	 foreach( $stmt as $row)
 	 {
 		 echo $row["name"];
 		 echo $row["msg"];
 		 }
-	 echo "df";
+	
 	
