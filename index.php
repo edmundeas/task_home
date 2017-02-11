25350c6af73241fe0a83ae27fe3a2ca1e11e503a
Ilya <edmundeas@gmail.com>
2017-02-11 06:12:41 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
-// First task - "Hello, World!"
-
-echo "Hello, World!";
+	/**
+	 * 
+	 *  Чат с использованием PDO
+	 * 
+	 * 
+	 */
+	 
+	 $usr = "root";
+	 $pswrd = "123trus";
+	 $config = array(PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC);
+	 $db_connect = new PDO('mysql:host=localhost;dbname=cms', $usr,$pswrd,$config);
+	 $stmt = $db_connect->query('SELECT* From user,message');
+	 foreach( $stmt as $row)
+	 {
+		 echo $row["name"];
+		 echo $row["msg"];
+		 }
+	 echo "df";
+	
